92570ef30eef196ad4db77b21881db575160c37e
Rajeshghb555 <raj.gajula555@gmail.com>
2017-11-03 14:56:05 +0530

    Add All Files

---
+++ load_second.php
@@ -0,0 +1,21 @@
+<?php
+include('config.php');
+
+$last_msg_id=$_GET['last_msg_id'];
+ $sql=mysqli_query($bd,"SELECT * FROM messages WHERE mes_id < '$last_msg_id' ORDER BY mes_id DESC LIMIT 5");
+ $last_msg_id="";
+
+		while($row=mysqli_fetch_array($sql))
+		{
+		$msgID= $row['mes_id'];
+		$msg= $row['msg'];
+	?>
+		
+		<div id="<?php echo $msgID; ?>"  align="left" class="message_box" >
+<span class="number"><?php echo $msgID; ?></span><?php echo $msg; ?> 
+</div>
+		
+
+<?php
+}
+?>
---
+++ request.php
@@ -0,0 +1,21 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "consightdev";
+$limitStart = $_POST['limitStart'];
+$limitCount = 100;
+if(isset($limitStart ) || !empty($limitStart)) {
+$con = mysqli_connect($hostname, $username, $password, $dbname);
+$query = "SELECT CURRENT_TEMPERATURE_TABLE_ID, TEMPERATURE,CUR_TIME  FROM current_temperature_inf limit $limitStart, $limitCount";
+$result = mysqli_query($con, $query);
+$res = array();
+while($resultSet = mysqli_fetch_assoc($result)) {
+
+$res[$resultSet['CURRENT_TEMPERATURE_TABLE_ID']] = array('temp'=>$resultSet['TEMPERATURE'],'cuti'=>$resultSet['CUR_TIME']);
+
+}
+echo json_encode($res);
+}
+?>
